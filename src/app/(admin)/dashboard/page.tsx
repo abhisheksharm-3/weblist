9e1c3e823173043eca7b8cbdfe372def831dd240
Abhishek Sharma <abhitiku2003@gmail.com>
2025-01-13 22:15:28 +0530

    Add AdminDashboard component with layout integration

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout/Layout'
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <Layout>AdminDashboard</Layout>
+  )
+}
+
+export default AdminDashboard

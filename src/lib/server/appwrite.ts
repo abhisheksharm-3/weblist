db03d39b2a69b13c0687820ed8e62797dc075047
Abhishek Sharma <abhitiku2003@gmail.com>
2025-01-23 19:50:26 +0530

    Add node-appwrite dependency and implement session client functions

---
+++ src/lib/server/appwrite.ts
@@ -0,0 +1,35 @@
+"use server";
+import { Client, Account } from "node-appwrite";
+import { cookies } from "next/headers";
+
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+    .setProject(process.env.APPWRITE_PROJECT_ID!);
+  const weblistCookies = await cookies();
+  const session = weblistCookies.get("weblist-session");
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+    .setProject(process.env.APPWRITE_PROJECT_ID!)
+    .setKey(process.env.APPWRITE_API_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
